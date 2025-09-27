cdc8879424b6b71b3edec002922a4a2904935337
Csancheza34 <carlosdavidsanchez2003@gmail.com>
2025-09-27 00:22:14 +0000

    chore: Renombra CSS para forzar limpieza de caché

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React, { useState } from 'react';
-import './App.css'
+import './App.styles.css';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
 import './components/Header.css'
